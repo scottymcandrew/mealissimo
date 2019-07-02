402d8c97c848058d3dc0fcfb511055c80c5b053e
Scott McAndrew <scottymcandrew@outlook.com>
2019-07-02 19:26:56 +0100

    Formatting improvements, random recipe selection, list remaining recipe hits as links

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,7 @@
 // "https://api.edamam.com/search?q=chicken&app_id=6047f138&app_key=fccffc05eb1ab43b73f574ec0ffdab5a&from=0&to=3&calories=591-722&health=alcohol-free"
 // Multiple ingredients should be separated with +
 // Like this: q=chicken+mushroom
+// Maximum hits is 100 on the free plan. Pagination isn't possible with the search API
 
 const recipeApiEndpoint = "https://api.edamam.com/search?";
 const recipeApiKeyID = "6047f138";
@@ -16,7 +17,7 @@ var data;
 // This is a callback function, expecting the parameter "cb", which is another function.
 function getRecipeData(params, cb) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", recipeApiEndpoint + "q=" + params + "&app_id=6047f138&app_key=fccffc05eb1ab43b73f574ec0ffdab5a&from=0&to=3&calories=591-722&health=alcohol-free");
+    xhr.open("GET", recipeApiEndpoint + "q=" + params + "&app_id=6047f138&app_key=fccffc05eb1ab43b73f574ec0ffdab5a&from=0&to=100&calories=591-722&health=alcohol-free");
     xhr.send();
 
     xhr.onreadystatechange = function() {
@@ -28,35 +29,6 @@ function getRecipeData(params, cb) {
     };
 }
 
-function getTableHeaders(obj) {
-    var tableHeaders = [];
-
-    Object.keys(obj).forEach(function(key) {
-        tableHeaders.push(`<td>${key}</td>`);
-    });
-
-    return `<tr>${tableHeaders}</tr>`;
-}
-
-function getTableRows(obj) {
-    var tableRows = [];
-
-    Object.values(obj).forEach(function(item) {
-        tableRows.push(`<td>${item}</td>`);
-    });
-
-    return tableRows;
-}
-
-function getTableEntries(obj) { // This function is a combination of the two get headers/row functions above
-    var tableEntries = [];
-
-    Object.entries(obj).forEach(function(item) {
-        tableEntries.push(`<td>${item}</td>`);
-    });
-
-    return tableEntries;
-}
 
 function getRecipeIngredients(arr) {
     var recipeIngredients = [];
@@ -70,8 +42,14 @@ function getRecipeIngredients(arr) {
     
 }
 
-function getRecipeMethod(obj) {
-    var recipeMethod = [];
+function getMoreRecipeLinks(arr) {
+    var recipeLinks = [];
+
+    arr.forEach(function(item) {
+        recipeLinks.push(`<li><a href="${item.recipe.url}" target="_blank"> ${item.recipe.label} </a></li>`)
+    });
+
+    return recipeLinks;
 }
 
 function writeToDocument(params) {
@@ -82,27 +60,31 @@ function writeToDocument(params) {
     var elRecipeImage = document.getElementById("recipe-image");
     var elRecipeIngredients = document.getElementById("recipe-ingredients");
     var elRecipeLink = document.getElementById("recipe-link");
+    var elOtherRecipeLinks = document.getElementById("other-recipe-links");
     // elData.innerHTML = "";
     // elRecipeHeader.innerHTML = "";
     elRecipeLabel.innerHTML = "";
     elRecipeImage.innerHTML = "";
     elRecipeIngredients.innerHTML = "";
     elRecipeLink.innerHTML = "";
+    elOtherRecipeLinks.innerHTML = "";
 
     getRecipeData(params, function(data) {
         console.dir(data); // Adding telemetry during dev
         // data = data.hits; // hits is the array containing the recipes
-        var recipe = data.hits[0].recipe; // return the first recipe Object
-        var recipeIngredients = getRecipeIngredients(data.hits[0].recipe.ingredientLines);
-        // var tableHeaders = getTableHeaders(data[0].recipe);
-        // var tableRows = getTableRows(data[0].recipe);
-        //elData.innerHTML = `<table>${tableHeaders}${tableRows}</table>`
+        var recipeLinks = data.hits;
+        console.dir(recipeLinks);
+        var recipeNumber = Math.floor(Math.random() * 101);  // returns a random integer from 0 to 100 to be used to randomise the first recipe hit.
+        var recipe = data.hits[recipeNumber].recipe; // return the selected recipe Object
+        var recipeIngredients = getRecipeIngredients(data.hits[recipeNumber].recipe.ingredientLines);
+        var moreRecipeLinks = getMoreRecipeLinks(data.hits);
 
         elRecipeLabel.innerHTML = `<h4>${recipe.label}</h4>`;
         elRecipeImage.innerHTML = `<img src="${recipe.image}"/>`;
         elRecipeIngredients.innerHTML = `<ul><h2 class="header-text">Ingredients</h2>${recipeIngredients}</ul>`.replace(/,/g, `<br>`);
-        elRecipeLink.innerHTML = `<a href="${recipe.url}" target="_blank">Want to cook this?</a>`;
-        
+        elRecipeLink.innerHTML = `<a href="${recipe.url}" target="_blank">Take me to the magic!</a>`;
+
+        elOtherRecipeLinks.innerHTML = `<h4>This doesn't tickle your taste-buds?</h4><p>Try one of these...</p><ul>${moreRecipeLinks}</ul>`.replace(/,/g, `<br>`);
 
     });
 
