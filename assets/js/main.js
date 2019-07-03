714c136513ae210c19b0d6411874359731b171af
Scott McAndrew <scottymcandrew@outlook.com>
2019-07-03 20:52:30 +0100

    Added more search parameters and some parameter checking

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,12 +15,12 @@ var data;
 
 // Function call to return data from Recipe API
 // This is a callback function, expecting the parameter "cb", which is another function.
-function getRecipeData(params, cb) {
+function getRecipeData(params, cals, excluded, health, cb) {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", recipeApiEndpoint + "q=" + params + "&app_id=6047f138&app_key=fccffc05eb1ab43b73f574ec0ffdab5a&from=0&to=100&calories=591-722&health=alcohol-free");
+    xhr.open("GET", recipeApiEndpoint + "q=" + params + "&calories=" + cals + "&excluded" + excluded + "&app_id=6047f138&app_key=fccffc05eb1ab43b73f574ec0ffdab5a&from=0&to=100&health=alcohol-free");
     xhr.send();
 
-    xhr.onreadystatechange = function() {
+    xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             // Here we are executing the callback function which was passed in as a parameter
             // In this case, it is 'printDataToConsole'
@@ -34,25 +34,25 @@ function getRecipeIngredients(arr) {
     var recipeIngredients = [];
 
     //Object.values(obj).forEach(function(item) {
-    arr.forEach(function(item) {
+    arr.forEach(function (item) {
         recipeIngredients.push(`<li>${item}</li>`);
     });
 
     return recipeIngredients;
-    
+
 }
 
 function getMoreRecipeLinks(arr) {
     var recipeLinks = [];
 
-    arr.forEach(function(item) {
+    arr.forEach(function (item) {
         recipeLinks.push(`<li><a href="${item.recipe.url}" target="_blank"> ${item.recipe.label} </a></li>`)
     });
 
     return recipeLinks;
 }
 
-function writeToDocument(params) {
+function writeToDocument(params, cals, excluded, health) {
     // Clear div elements to prevent concatenation on subsequent form fills
     // var elData = document.getElementById("data");
     // var elRecipeHeader = document.getElementById("recipe-header");
@@ -69,24 +69,37 @@ function writeToDocument(params) {
     elRecipeLink.innerHTML = "";
     elOtherRecipeLinks.innerHTML = "";
 
-    getRecipeData(params, function(data) {
-        console.dir(data); // Adding telemetry during dev
-        // data = data.hits; // hits is the array containing the recipes
-        var recipeLinks = data.hits;
-        console.dir(recipeLinks);
-        var recipeNumber = Math.floor(Math.random() * 101);  // returns a random integer from 0 to 100 to be used to randomise the first recipe hit.
-        var recipe = data.hits[recipeNumber].recipe; // return the selected recipe Object
-        var recipeIngredients = getRecipeIngredients(data.hits[recipeNumber].recipe.ingredientLines);
-        var moreRecipeLinks = getMoreRecipeLinks(data.hits);
+    if (cals > 49) {
 
-        elRecipeLabel.innerHTML = `<h4>${recipe.label}</h4>`;
-        elRecipeImage.innerHTML = `<img src="${recipe.image}"/>`;
-        elRecipeIngredients.innerHTML = `<ul><h2 class="header-text">Ingredients</h2>${recipeIngredients}</ul>`.replace(/,/g, `<br>`);
-        elRecipeLink.innerHTML = `<a href="${recipe.url}" target="_blank">Take me to the magic!</a>`;
+        getRecipeData(params, cals, excluded, health, function (data) {
+            if (data.hits.length > 0) {
+                console.dir(data); // Adding telemetry during dev
+                // data = data.hits; // hits is the array containing the recipes
+                // var recipeLinks = data.hits;
+                // console.dir(recipeLinks);
+                var recipeCount = data.hits.length;
+                var recipeNumber = Math.floor(Math.random() * recipeCount);  // returns a random integer from 0 to number of recipes (up to 100) to be used to randomise the first recipe hit.
+                var recipe = data.hits[recipeNumber].recipe; // return the selected recipe Object
+                var recipeIngredients = getRecipeIngredients(data.hits[recipeNumber].recipe.ingredientLines);
+                var moreRecipeLinks = getMoreRecipeLinks(data.hits);
 
-        elOtherRecipeLinks.innerHTML = `<h4>This doesn't tickle your taste-buds?</h4><p>Try one of these...</p><ul>${moreRecipeLinks}</ul>`.replace(/,/g, `<br>`);
+                elRecipeLabel.innerHTML = `<h2>${recipe.label}</h2>`;
+                elRecipeImage.innerHTML = `<img src="${recipe.image}"/>`;
+                elRecipeIngredients.innerHTML = `<ul><h2 class="header-text">Ingredients</h2>${recipeIngredients}</ul>`.replace(/,/g, `<br>`);
+                elRecipeLink.innerHTML = `<button class="btn btn-primary"><a href="${recipe.url}" target="_blank">Take me to the magic!</a></button>`;
 
-    });
-
-
+                elOtherRecipeLinks.innerHTML = `<h4>This doesn't tickle your taste-buds?</h4><p><em>Try one of these...</em></p><ul>${moreRecipeLinks}</ul>`.replace(/,/g, `<br>`);
+            }
+            else {
+                elRecipeLabel.innerHTML = `<h2>Uh oh......<h2>`;
+                elRecipeImage.innerHTML = `<img src="assets/images/corgi-food.jpg"/>`;
+                elRecipeLink.innerHTML = `<p>Looks like our cute Corgi will go hungry. Maybe you need to search for something else?`;
+            }
+        });
+    }
+    else {
+        elRecipeLabel.innerHTML = `<h2>Uh oh......<h2>`;
+        elRecipeImage.innerHTML = `<img src="assets/images/wet-corgi.jpg"/>`;
+        elRecipeLink.innerHTML = `<p>Our Corgi is too thin. We need to add more calories!`;
+    }
 }
